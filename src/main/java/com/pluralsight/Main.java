6f6d3285013b0f9bfc7648909b12dab339d70035
RickelviC <Cruzrickelvi@gmail.com>
2026-05-05 11:01:17 -0400

    cleaning room and making sure its not occupied

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,18 @@
 package com.pluralsight;
 
 public class Main {
+    public static void main(String[] args) {
+        Room room = new Room(2,99);
+        System.out.println("Number of Beds: " + room.getNumberOfBeds());
+        System.out.println("Price: " + room.getPrice());
+        System.out.println("Is Occupied: " + room.isOccupied());
+        System.out.println("Is Dirty: " + room.isDirty());
+        System.out.println("Is Available: " + room.isAvailable());
+        System.out.println("========================================================");
+
+        if (room.checkIn()){
+            room.checkOut();
+        }
+        System.out.println("Is Available: " + room.isAvailable());
+    }
 }
